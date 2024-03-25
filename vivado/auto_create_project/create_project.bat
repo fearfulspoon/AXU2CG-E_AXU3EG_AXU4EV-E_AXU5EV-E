1c0efc9a4f95a833a18f6fa8fc6a9694f87628b4
FearfulSpoon <37189490+fearfulspoon@users.noreply.github.com>
2024-03-25 18:44:07 -0500

    Mods for our system.

--- vivado/auto_create_project/create_project.bat
+++ vivado/auto_create_project/create_project.bat
@@ -1,2 +1,3 @@
-CALL E:\XilinxVitis\Vivado\2020.1\bin\vivado.bat -mode batch -source create_project.tcl
+CALL C:\Xilinx\Vivado\2023.1\bin\vivado.bat -mode batch -source create_project.tcl
+PAUSE create_project.tcl
 PAUSE
